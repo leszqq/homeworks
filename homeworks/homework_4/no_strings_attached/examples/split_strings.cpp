67c5ed4c038376c8434e44480e3a81bcf21a7396
wiktor <comradewiktor@gmail.com>
2023-12-17 16:31:16 +0100

    Add split string example

---
+++ homeworks/homework_4/no_strings_attached/examples/split_strings.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+#include "no_strings_attached/split_string.h"
+
+using std::cin;
+using std::cout;
+using std::getline;
+using std::string;
+using std::string_literals::operator""s;
+using std::vector;
+
+auto main() -> int {
+  cout << "Example program that splits strings.\n"s;
+  cout << "Please enter a string:\n"s;
+
+  string line{};
+  getline(cin, line);
+  vector<string> splitted{no_strings_attached::Split(line, " "s)};
+  cout << "Your split string:"s;
+  for (const auto &token : splitted) { cout << " '"s << token << "'"; }
+  return 0;
+}
