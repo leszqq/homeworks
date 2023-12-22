04ad84af17aeaed2fb7e41a8190d29e643980a02
wiktor <comradewiktor@gmail.com>
2023-12-22 01:10:00 +0100

    Create Image header and add image lib target

--- homeworks/homework_5/pixelator/pixelator/image.hpp
+++ homeworks/homework_5/pixelator/pixelator/image.hpp
@@ -1,3 +1,23 @@
+#include <cstdint>
+#include <ftxui/screen/color.hpp>
+#include <vector>
+
+#include "size.hpp"
+
 namespace pixelator {
-class Image {}
+class Image {
+ public:
+  Image() = default;
+  Image(int rows, int cols);
+
+  // getters
+  [[nodiscard]] auto empty() const noexcept -> bool;
+  [[nodiscard]] auto rows() const noexcept -> int;
+  [[nodiscard]] auto cols() const noexcept -> int;
+  [[nodiscard]] auto size() const noexcept -> Size;
+  [[nodiscard]] auto at(int row, int col) const -> ftxui::Color &;
+
+ private:
+  std::vector<std::vector<ftxui::Color>> data_{};
+};
 }  // namespace pixelator
